78f799a26f78b3ed0a09140c054b19f70926865a
formidablae <81068781+formidablae@users.noreply.github.com>
2022-03-31 22:55:22 +0200

    Minor command fixes

--- Chapter_03_Data_Modeling_in_Flask/03_Creating_a_relational_category_model/commands.sh
+++ Chapter_03_Data_Modeling_in_Flask/03_Creating_a_relational_category_model/commands.sh
@@ -12,9 +12,12 @@ python3.8 -m pip install requests
 
 # open in browser
 xdg-open "http://127.0.0.1:5000/products"
+xdg-open "http://127.0.0.1:5000/categories"
 python3.8
 import requests
-requests.post("https://127.0.0.1:5000/product-create", data ={"name": "iPhone 5S", "price": "549.0"})
+requests.post("https://127.0.0.1:5000/category-create", data ={"name": "Phones"})
+requests.post("https://127.0.0.1:5000/product-create", data ={"name": "iPhone 5S", "price": "549.0", "category": "Phones"})
 
 # open in browser again to see the added product
+xdg-open "http://127.0.0.1:5000/categories"
 xdg-open "http://127.0.0.1:5000/products"
